c6aa6b74e6f284f731baab4c122a2285cfaa550b
Dominick <dominickveil@gmail.com>
2020-01-18 12:09:30 +0100

    Add jsdoc intro

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-import {App} from './src/app'
+/**
+ * Hi, thank you for checking out my repository.
+ * I'm always looking for ways to improve, so i would really appreciate any kind of feedback.
+ *
+ * @copyright Dominick Vale 2020
+ */
+
 
 // eslint-disable-next-line no-unused-vars
 import main from './styles/main.sass'
+import {App} from './src/app'
 
 window.addEventListener('load', () => {
   App.init()
