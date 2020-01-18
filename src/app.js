5a88f2276f50f363c7d4cc47aa0e0a2b07cd2e12
Dominick <dominickveil@gmail.com>
2020-01-18 12:11:59 +0100

    Wait while fetching Pokemons + utility functions +  jsdoc comments

--- src/app.js
+++ src/app.js
@@ -1,25 +1,43 @@
+/* eslint-disable no-useless-escape */
+import GiratinaImage from '../assets/img/giratina.png'
+
 const DEBUG = true
 const log = (...args) => DEBUG && console.info.apply(console, args)
 
+
 const ButtonCheck   = document.getElementById('check-button')
+const ImagePokemon  = document.getElementById('img-pokemon')
+const Spinner       = document.getElementById('spinner')
 const TextboxHeight = document.getElementById('textbox-height')
 const TextboxWeight = document.getElementById('textbox-weight')
 const Title         = document.getElementById('h1-title')
-const ImagePokemon  = document.getElementById('img-pokemon')
-const Spinner       = document.getElementById('spinner')
 
-const API_URL = 'https://pokeapi.co/api/v2/'
+const API_URL         = 'https://pokeapi.co/api/v2/'
 const SPRITES_API_URL = 'https://pokeres.bastionbot.org/images/pokemon/'
-const STORAGE_KEY = 'pokemonsList'
+const STORAGE_KEY     = 'pokemonsList'
+const API_LIMIT = 964
+
+let pokemons = []
+
 /**
  * @todo 
- * Implement caching of pokemon list and images
+ * The app is not going to be able to work because of the inevitable 964 requests it has to start
+ * in order to fetch all the Pokèmon and list for them
  * (optional) add accuracy slidebar
  */
 
 
 
-// Just sorts by closest matching entry. Entry with the smaller difference from the input bubbles up
+const setTitle = (text) => Title.textContent = text
+const toggleSpinner = () => Spinner.className === '' ? Spinner.className = 'd-none' : Spinner.className = ''  
+
+
+/**
+ * Just sorts by closest matching entry. Entry with the smaller difference from the input bubbles up.
+ * @param {array} array - The array to sort
+ * @param {number} height - The input height
+ * @param {number} weight - The input weight
+ */
 const sortByDifference = (array, height, weight) =>{
   array.sort((pokemonA, pokemonB) => 
   Math.abs(height - pokemonA.height) > Math.abs(height - pokemonB.height) 
@@ -27,17 +45,29 @@ const sortByDifference = (array, height, weight) =>{
 }
 
 
-const fetchPokemons = async () =>{
+
+/**
+ * Fetches the Pokèmon list, then fills the result with objects containing the name, height, weight and sprite of each of them.
+ * Then waits until all the promises are fulfilled.
+ * @async
+ * @returns {(promise|array)} the array of Pokèmons. 
+ */
+const _fetchPokemons = async () =>{
   let result = []
+  let promises = []
+
   if(!window.localStorage.getItem(STORAGE_KEY)){
+    setTitle('Fetching Pokèmons...')
+    toggleSpinner()
     try{
-      const response = await fetch(`${API_URL}pokemon/?limit=200`).catch(error => console.error('Error in fetching pokèmon list: ', error))
+      const response = await fetch(`${API_URL}pokemon/?limit=${API_LIMIT}`)
       const data = await response.json()
 
       for(let entry of data.results){
         const pokemon = await fetch(entry.url)
         const {id, weight, height} = await pokemon.json()
-      
+        promises.push(pokemon)
+
         result.push({
             name: entry.name,
             weight: weight / 10,
@@ -48,22 +78,31 @@ const fetchPokemons = async () =>{
       window.localStorage.setItem(STORAGE_KEY, JSON.stringify(result))
 
     }
-    catch(error) {  DEBUG && console.error('Error in fetching pokèmon stats: ', error)  }
+    catch(error) {  DEBUG && console.error('Error in fetching pokèmon list: ', error)  }
+
+    await Promise.all(promises)  // wait for all the requests to be fullfilled
+    setTitle('Input your height and weight!')
+    toggleSpinner()
   } else {
     log('Retrieving local storage data...')
     result = JSON.parse(window.localStorage.getItem(STORAGE_KEY))
   }
 
-  return result || 'No Pokèmons found...'
+  return result
 }
 
 
+
+/**
+ * Finds the correct Pokèmon by traversing the list and calculating the difference of the input height and weight against Pokèmons.
+ * @param {number} [Height=0] - The input height
+ * @param {number} [Weight=0] - The input weight
+ * @returns {object} the best matching Pokèmon.
+ */
 const findPokemon = async (height = 0, weight = 0) => {
   const results = []
   const MAX_RESULTS = 2
   const MAX_OFFSET = 30
-
-  let pokemons = await fetchPokemons()
 
   for(let pokemon of pokemons){
     if(Math.abs(height - pokemon.height) <= MAX_OFFSET && Math.abs(weight - pokemon.weight) <= MAX_OFFSET){ // Didn't want to use ternary for code legibility
@@ -79,33 +118,58 @@ const findPokemon = async (height = 0, weight = 0) => {
 }
 
 
-const onCheckButtonClick = async () => {
-  const height = TextboxHeight.value
-  const weight = TextboxWeight.value
-  Title.textContent = '...'
-  ImagePokemon.className = 'img-fluid responsive__image invisible'
-  Spinner.className = ''
-  
-  const matchingPokemon = await findPokemon(height, weight)
-  log('Matching pokemon: ', matchingPokemon)
-  
-  Title.textContent = matchingPokemon.name
-  ImagePokemon.setAttribute('src', matchingPokemon.sprite)
-  ImagePokemon.setAttribute('alt', `An image of ${matchingPokemon.name}`)
-  ImagePokemon.className = 'img-fluid responsive__image'
-  Spinner.className = 'd-none'
+
+/**
+ * Checks if the input is a number or the key 'Enter'. 
+ * 
+ * When 'Enter' is pressed submit the form...
+ * (Note: this is a workaround. Forms should automatically have this functionality but i don't know why it's not working)
+ */
+const inputValidation = (evt) => {
+  if (evt.keyCode == 13 && TextboxHeight.value + TextboxWeight.value >= 2) ButtonCheck.click()
+  if(!(evt.charCode >= 48 && evt.charCode <= 57)){
+    evt.preventDefault()
+  }
 }
 
 
-const onHeightInput = (evt) => log(evt)
-const onWeightInput = (evt) => log(evt)
 
-const _init = () => {
-  log('Initializing events')
+const onCheckButtonClick = async () => {
+  const height = TextboxHeight.value
+  const weight = TextboxWeight.value
+  setTitle('...')
+  ImagePokemon.className = 'img-fluid responsive__image invisible'
+  toggleSpinner()
+  
+  const matchingPokemon = await findPokemon(height, weight)
+  log('Matching pokemon: ', matchingPokemon)
+  toggleSpinner()
 
+  if(matchingPokemon){ 
+    Title.className += 'text-center mx-auto'
+    setTitle(`You are ${matchingPokemon.name}!`)
+    ImagePokemon.setAttribute('src', matchingPokemon.sprite)
+    ImagePokemon.setAttribute('alt', `An image of ${matchingPokemon.name}`)
+    ImagePokemon.className = 'img-fluid responsive__image'
+  } else {
+    Title.innerHTML = 'No Pokèmon found... Try&nbsp;again!'
+    Title.className += ' text-danger'
+    ImagePokemon.className = 'img-fluid responsive__image'
+    ImagePokemon.setAttribute('alt', 'The mighty giratina weighing 750 kilograms!')
+    ImagePokemon.setAttribute('src', GiratinaImage)
+  }
+}
+
+
+
+
+
+
+const _init = async () => {
   ButtonCheck.addEventListener('click', onCheckButtonClick)
-  TextboxHeight.addEventListener('keypress', onHeightInput)
-  TextboxWeight.addEventListener('keypress', onWeightInput)
+  TextboxHeight.addEventListener('keypress', inputValidation)
+  TextboxWeight.addEventListener('keypress', inputValidation)
+  pokemons = await _fetchPokemons()
 }
 
 
