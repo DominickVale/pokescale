ae9eafc915a4a7b5998d47e4d2931b9c9995634d
Frank <fj@move-elevator.de>
2020-01-19 21:42:18 +0100

    restructure static files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,21 +1,27 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
-  entry: './index.js',
   plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      { from: 'public', to: './' }
+    ]),
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './index.html',
+      template: './public/index.html',
     }),
     new HtmlWebpackPlugin({
       filename: 'app.html',
-      template: './src/app.html',
+      template: './public/app.html',
     }),
     new WorkboxPlugin.GenerateSW({
       swDest: 'sw.js',
       skipWaiting: true,
       clientsClaim: true
+
     }),
   ],
   module: {
