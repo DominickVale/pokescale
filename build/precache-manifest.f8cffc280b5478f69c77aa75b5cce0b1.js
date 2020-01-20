a10445f565144e8b480d9374ae4f3609c1bd15ab
Dominick <dominickveil@gmail.com>
2020-01-20 11:30:13 +0100

    Stable (1.0.4)

---
+++ build/precache-manifest.f8cffc280b5478f69c77aa75b5cce0b1.js
@@ -0,0 +1,48 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457319b183963e4efd5354caa55da865",
+    "url": "app.html"
+  },
+  {
+    "revision": "1888b71aa651100801cb2e473433f5ec",
+    "url": "assets/fonts/Nintendo-DS-BIOS.ttf"
+  },
+  {
+    "revision": "1cfce8048347e1d3c3d6e2c756a817a1",
+    "url": "assets/img/giratina.png"
+  },
+  {
+    "revision": "30ad08ea6ac375b65289a503dfc1c5d0",
+    "url": "assets/img/icon_192.png"
+  },
+  {
+    "revision": "1c37dc6cac49da72a0f901250109ee49",
+    "url": "assets/img/icon_512.png"
+  },
+  {
+    "revision": "86be15a1385827d4f24ee929d85e3c97",
+    "url": "assets/pokemonsList.json"
+  },
+  {
+    "url": "fonts/Nintendo-DS-BIOS.1888b71aa651100801cb2e473433f5ec.ttf"
+  },
+  {
+    "url": "imgs/giratina.1cfce8048347e1d3c3d6e2c756a817a1.png"
+  },
+  {
+    "revision": "bbcee3a81df783e5e7eeba19daf00486",
+    "url": "index.html"
+  },
+  {
+    "revision": "b356485f9120291c022c",
+    "url": "main.5e1819d5dc484a0ab1ae.css"
+  },
+  {
+    "revision": "b356485f9120291c022c",
+    "url": "main.c4cce5b2feab9b9aa086.js"
+  },
+  {
+    "revision": "4a7a169b651af7edcd830c8eb708da8e",
+    "url": "manifest.json"
+  }
+]);
